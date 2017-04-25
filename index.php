421bab60b90339016a8ee9f0a9a6d38255568586
wihskas <lesberetsverts@outlook.com>
2017-04-25 10:10:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Docker</title>
+</head>
+
+<body>
+    <h1>Voici les informations des conteneurs:</h1>
+
+       ID du conteneur: <?php echo exec('cat /etc/hostname'); ?>
+       
+
+</body>
+
+</html>
